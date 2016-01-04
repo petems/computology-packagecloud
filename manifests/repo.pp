afb38f65a3bb1da102dc48e56004a159c6bf7989
Kyrill Poole <kyrill@pancentric.com>
2016-01-04 16:39:24 +0000

    Refactoring how apt repos are managed

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -28,7 +28,9 @@ define packagecloud::repo(
   validate_string($type)
   validate_string($master_token)
 
+
   include ::packagecloud
+  include ::apt
 
   if $fq_name != undef {
     $repo_name = $fq_name
@@ -62,50 +64,48 @@ define packagecloud::repo(
           $repo_url = "${base_url}/${repo_name}/${osname}"
           $distribution =  $::lsbdistcodename
 
-          file { $normalized_name:
-            ensure  => file,
-            path    => "/etc/apt/sources.list.d/${normalized_name}.list",
-            mode    => '0644',
-            content => template('packagecloud/apt.erb'),
+          apt::source {$normalized_name:
+            location => $repo_url,
+            repos    => $component,
+            key      => {
+              'id'     => '418A7F2FB0E1E6E7EABF6FE8C2E73424D59097AB',
+              'server' => 'pgp.mit.edu',
+            },
           }
 
-          exec { "apt_key_add_${normalized_name}":
-            command => "wget --auth-no-challenge -qO - ${base_url}/${repo_name}/gpgkey | apt-key add -",
-            path    => '/usr/bin/:/bin/',
-            require => File[$normalized_name],
+          default: {
+            fail("Sorry, ${::operatingsystem} isn't supported for apt repos at this time. Email support@packagecloud.io")
           }
-
-          exec { "apt_get_update_${normalized_name}":
-            command =>  "apt-get update -o Dir::Etc::sourcelist=\"sources.list.d/${normalized_name}.list\" -o Dir::Etc::sourceparts=\"-\" -o APT::Get::List-Cleanup=\"0\"",
-            path    => '/usr/bin/:/bin/',
-            require => Exec["apt_key_add_${normalized_name}"],
-          }
-        }
-        default: {
-          fail("Sorry, ${::operatingsystem} isn't supported for apt repos at this time. Email support@packagecloud.io")
         }
       }
-    }
-    'rpm': {
-      case $::operatingsystem {
-        'RedHat', 'redhat', 'CentOS', 'centos', 'Amazon', 'Fedora', 'Scientific', 'OracleLinux', 'OEL': {
+      'rpm': {
+        case $::operatingsystem {
+          'RedHat', 'redhat', 'CentOS', 'centos', 'Amazon', 'Fedora', 'Scientific', 'OracleLinux', 'OEL': {
 
-          $majrel = $::osreleasemaj
-          if $::pygpgme_installed == 'false' {
-            warning('The pygpgme package could not be installed. This means GPG verification is not possible for any RPM installed on your system. To fix this, add a repository with pygpgme. Usualy, the EPEL repository for your system will have this. More information: https://fedoraproject.org/wiki/EPEL#How_can_I_use_these_extra_packages.3F and https://github.com/stahnma/puppet-module-epel')
-            $repo_gpgcheck = 0
-          } else {
-            $repo_gpgcheck = 1
-          }
+            $majrel = $::osreleasemaj
+            if $::pygpgme_installed == false {
+              warning('The pygpgme package could not be installed. This means GPG verification is not possible for any RPM installed on your system. To fix this, add a repository with pygpgme. Usualy, the EPEL repository for your system will have this. More information: https://fedoraproject.org/wiki/EPEL#How_can_I_use_these_extra_packages.3F and https://github.com/stahnma/puppet-module-epel')
+              $repo_gpgcheck = 0
+            } else {
+              $repo_gpgcheck = 1
+            }
 
-          if $read_token {
-            if $majrel == '5' {
-              $yum_repo_url = $::operatingsystem ? {
-                /(RedHat|redhat|CentOS|centos)/ => "${server_address}/priv/${read_token}/${repo_name}/el/5/${::architecture}/",
-                /(OracleLinux|OEL)/ => "${server_address}/priv/${read_token}/${repo_name}/ol/5/${::architecture}/",
-                'Scientific' => "${server_address}/priv/${read_token}/${repo_name}/scientific/5/${::architecture}/",
+            if $read_token {
+              if $majrel == '5' {
+                $yum_repo_url = $::operatingsystem ? {
+                  /(RedHat|redhat|CentOS|centos)/ => "${server_address}/priv/${read_token}/${repo_name}/el/5/${::architecture}/",
+                  /(OracleLinux|OEL)/ => "${server_address}/priv/${read_token}/${repo_name}/ol/5/${::architecture}/",
+                  'Scientific' => "${server_address}/priv/${read_token}/${repo_name}/scientific/5/${::architecture}/",
+                }
+                $gpg_url = "${server_address}/priv/${read_token}/${repo_name}/gpgkey"
+              } else {
+                $yum_repo_url = $::operatingsystem ? {
+                  /(RedHat|redhat|CentOS|centos)/ => "${base_url}/${repo_name}/el/${majrel}/${::architecture}/",
+                  /(OracleLinux|OEL)/ => "${base_url}/${repo_name}/ol/${majrel}/${::architecture}/",
+                  'Scientific' => "${base_url}/${repo_name}/scientific/${majrel}/${::architecture}/",
+                }
+                $gpg_url = "${base_url}/${repo_name}/gpgkey"
               }
-              $gpg_url = "${server_address}/priv/${read_token}/${repo_name}/gpgkey"
             } else {
               $yum_repo_url = $::operatingsystem ? {
                 /(RedHat|redhat|CentOS|centos)/ => "${base_url}/${repo_name}/el/${majrel}/${::architecture}/",
@@ -114,44 +114,36 @@ define packagecloud::repo(
               }
               $gpg_url = "${base_url}/${repo_name}/gpgkey"
             }
-          } else {
-            $yum_repo_url = $::operatingsystem ? {
-              /(RedHat|redhat|CentOS|centos)/ => "${base_url}/${repo_name}/el/${majrel}/${::architecture}/",
-              /(OracleLinux|OEL)/ => "${base_url}/${repo_name}/ol/${majrel}/${::architecture}/",
-              'Scientific' => "${base_url}/${repo_name}/scientific/${majrel}/${::architecture}/",
+
+            $description = $normalized_name
+            $repo_url = $::operatingsystem ? {
+              /(RedHat|redhat|CentOS|centos|Scientific|OracleLinux|OEL)/ => $yum_repo_url,
+              'Fedora' => "${base_url}/${repo_name}/fedora/${majrel}/${::architecture}/",
+              'Amazon' => "${base_url}/${repo_name}/el/6/${::architecture}",
             }
-            $gpg_url = "${base_url}/${repo_name}/gpgkey"
+
+            file { $normalized_name:
+              ensure  => file,
+              path    => "/etc/yum.repos.d/${normalized_name}.repo",
+              mode    => '0644',
+              content => template('packagecloud/yum.erb'),
+            }
+
+            exec { "yum_make_cache_${repo_name}":
+              command => "yum -q makecache -y --disablerepo='*' --enablerepo='${normalized_name}'",
+              path    => '/usr/bin',
+              require => File[$normalized_name],
+            }
           }
 
-          $description = $normalized_name
-          $repo_url = $::operatingsystem ? {
-            /(RedHat|redhat|CentOS|centos|Scientific|OracleLinux|OEL)/ => $yum_repo_url,
-            'Fedora' => "${base_url}/${repo_name}/fedora/${majrel}/${::architecture}/",
-            'Amazon' => "${base_url}/${repo_name}/el/6/${::architecture}",
+          default: {
+            fail("Sorry, ${::operatingsystem} isn't supported for yum repos at this time. Email support@packagecloud.io")
           }
-
-          file { $normalized_name:
-            ensure  => file,
-            path    => "/etc/yum.repos.d/${normalized_name}.repo",
-            mode    => '0644',
-            content => template('packagecloud/yum.erb'),
-          }
-
-          exec { "yum_make_cache_${repo_name}":
-            command => "yum -q makecache -y --disablerepo='*' --enablerepo='${normalized_name}'",
-            path    => '/usr/bin',
-            require => File[$normalized_name],
-          }
-        }
-
-        default: {
-          fail("Sorry, ${::operatingsystem} isn't supported for yum repos at this time. Email support@packagecloud.io")
         }
       }
+      default: {
+        fail("Sorry, ${type} isn't a supported repository type in this module right now. Email support@packagecloud.io")
+      }
     }
-    default: {
-      fail("Sorry, ${type} isn't a supported repository type in this module right now. Email support@packagecloud.io")
-    }
-  }
 
-}
+  }
